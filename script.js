0e86b760a52286275fb4be768ae865a055442137
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-14 15:37:08 +0545

    test all the functions

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 console.log("Thank you")
 // NepaliDate (year,month,date)
-let date1 = new NepaliDate(2054, 5, 24)
+let date1 = new NepaliDate(2060, 0, 23)
 
 // formatting
 var date2 = date1.format('ddd, DD MMMM YYYY') // 'Monday, 24 Aswin 2051'
-
+let date3 = date1.toJsDate();
 const datediv = document.getElementById("date")
-datediv.textContent = date2;
+datediv.textContent = date1;
+const dateConverted = document.querySelector(".convertedDate");
+dateConverted.textContent = date3;
